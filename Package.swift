fa3604089a17dbc71df77b0b4eb9eea2c87aeb9f
WebsoftProfession <websoftprofession@gmail.com>
2023-02-03 14:34:44 +0530

    package file updated

--- Package.swift
+++ Package.swift
@@ -24,6 +24,7 @@ let package = Package(
         .target(
             name: "WSCalendarViewSwift",
             dependencies: [],
+            sources: ["WSCalendarViewSwift/Classes/**/*"]
             path: "WSCalendarViewSwift/Classes")
     ]
 )
